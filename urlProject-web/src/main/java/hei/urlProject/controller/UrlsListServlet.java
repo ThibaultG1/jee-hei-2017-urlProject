094f542c8fbeac750fdb9f70a9f3dae0afae5eb6
ThibaultG1 <tiboyoyo@gmail.com>
2017-02-13 16:14:01 +0100

    first servlet

---
+++ urlProject-web/src/main/java/hei/urlProject/controller/UrlsListServlet.java
@@ -0,0 +1,38 @@
+package hei.urlProject.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static java.lang.System.out;
+
+/**
+ * Created by thiba on 13/02/2017.
+ */
+@WebServlet(urlPatterns = "/urlslist")
+public class UrlsListServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        out.println("<p>Hello</p>");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
