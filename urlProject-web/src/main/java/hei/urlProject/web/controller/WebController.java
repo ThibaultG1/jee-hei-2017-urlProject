c9cbb1e2d27540933157d006423dc8d422864c09
ThibaultG1 <tiboyoyo@gmail.com>
2017-02-25 12:53:52 +0100

    :D

--- urlProject-web/src/main/java/hei/urlProject/web/controller/WebController.java
+++ urlProject-web/src/main/java/hei/urlProject/web/controller/WebController.java
@@ -2,9 +2,13 @@ package hei.urlProject.web.controller;
 
 import hei.urlProject.core.entity.Url;
 import hei.urlProject.core.service.UrlService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.inject.Inject;
 import java.util.List;
@@ -12,14 +16,34 @@ import java.util.List;
 @Controller
 public class WebController {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(WebController.class);
+
     @Inject
     private UrlService urlService;
 
-    @RequestMapping("/")
+ /*   @RequestMapping({"/","index"})
     public String getListOfUrls(ModelMap model){
+        LOGGER.info("Je retourne la liste des URLs");
         List<Url> urls = urlService.findAll();
         model.addAttribute("urls",urls);
-        return "urlsList";
+        return "index";
+    }*/
+
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public String getFormAndList(ModelMap model){
+        LOGGER.info("J'affiche le formulaire d'ajout d'URL");
+        model.addAttribute("url", new Url());
+        LOGGER.info("Je retourne la liste des URLs");
+        List<Url> urls = urlService.findAll();
+        model.addAttribute("urls",urls);
+        return "index";
+    }
+
+    @RequestMapping(value="/form", method=RequestMethod.POST)
+    public String submitForm(@ModelAttribute("url") Url url){
+        LOGGER.info("J'ajoute un URL");
+        urlService.save(url);
+        return "redirect:";
     }
 
 }
