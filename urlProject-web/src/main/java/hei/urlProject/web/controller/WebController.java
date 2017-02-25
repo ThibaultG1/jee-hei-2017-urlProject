c76af3e13e2d1d49d973efeba3444ce7c967e64c
ThibaultG1 <tiboyoyo@gmail.com>
2017-02-25 22:21:30 +0100

    :0

--- urlProject-web/src/main/java/hei/urlProject/web/controller/WebController.java
+++ urlProject-web/src/main/java/hei/urlProject/web/controller/WebController.java
@@ -6,12 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class WebController {
@@ -21,25 +20,50 @@ public class WebController {
     @Inject
     private UrlService urlService;
 
-    @RequestMapping(value="/",method = RequestMethod.GET)
-    public String getFormAndList(ModelMap model){
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getFormAndList(ModelMap model) {
         LOGGER.info("J'affiche le formulaire d'ajout d'URL");
         model.addAttribute("url", new Url());
         LOGGER.info("Je retourne la liste des URLs");
         List<Url> urls = urlService.findAll();
-        model.addAttribute("urls",urls);
+        model.addAttribute("urls", urls);
         return "index";
     }
 
-    @RequestMapping(value="/form", method=RequestMethod.POST)
-    public String submitForm(@ModelAttribute("url") Url url){
+    @RequestMapping(value = "/form", method = RequestMethod.POST)
+    public String submitForm(@ModelAttribute("url") Url url) {
         LOGGER.info("J'ajoute un URL");
-        urlService.save(url);
-        return "redirect:";
+        if (url.getUrlRaccourci() == "") {
+            url.setUrlRaccourci(UUID.randomUUID().toString());
+            urlService.save(url);
+            return "redirect:";
+        } else {
+            if (urlService.findOneByUrlRaccourci(url.getUrlRaccourci()) == null) {
+                urlService.save(url);
+                return "redirect:";
+            }
+            else
+                return "redirect:nope";
+        }
+
+    }
+
+    @RequestMapping(value="/{urlRaccourci}")
+    public String redirectMe(@ModelAttribute("url") Url url) {
+        if (urlService.findOneByUrlRaccourci(url.getUrlRaccourci()) != null) {
+            return "redirect:" + urlService.findOneByUrlRaccourci(url.getUrlRaccourci()).getUrlInitial();
+        }
+        else
+            return "404";
+    }
+
+    @RequestMapping("/nope")
+    public String error() {
+        return "nope";
     }
 
     @RequestMapping("/*")
-    public String error(){
+    public String error404() {
         return "404";
     }
 
